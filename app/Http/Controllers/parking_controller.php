a482d5567cef1d910d22f49cc38e4e7a75bb51d5
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-03-10 10:30:56 +0000

    added the metod to add a new parking

--- app/Http/Controllers/parking_controller.php
+++ app/Http/Controllers/parking_controller.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\parking;
+use App\Models\user;
+use Illuminate\Validation\ValidationException;
 
 class parking_controller extends Controller
 {
-    //
+    public function addPrking(Request $req)
+    {
+        try {
+            $req->validate([
+                'name' => 'required|string|min:3|max:255',
+                "totale_spost" => 'required|numeric|gt:0',
+            ]);
+            $parkingSpot = parking::create([
+                'name' => $req->name,
+                'totale_spost' => $req->totale_spost,
+                "availabel_spots" => $req->totale_spost,
+            ]);
+            return response()->json(["message" => "new parking added successfully", "marpking" => $parkingSpot], 200);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'details' => $e->errors()
+            ], 422);
+        }
+    }
 }
